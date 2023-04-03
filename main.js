ab07ea28a79dacd25adb8a7689eb6e5989a1b31b
japman123 <87320271+japman123@users.noreply.github.com>
2023-04-03 19:51:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function preload() {
+img = loadImage('bed.jpeg');
+}
+
+function setup() {
+canvas = createCanvas(640, 420);
+canvas.center;
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "Status: Detecting Objects"
+}
+
+function modelLoaded() {
+    console.log('Model Loaded');
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if(error){
+        console.log(error);
+    }
+     console.log(results);
+}
